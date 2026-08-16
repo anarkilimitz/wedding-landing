412e3e3b25c770dfd422aa020fdcb22ae85d92cf
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-08-16 19:36:29 +0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+	return (
+		<main className="flex min-h-screen items-center justify-center bg-black">
+			<h1 className="text-6xl font-bold text-white">Wedding</h1>
+		</main>
+	);
+}
+
+export default App;
