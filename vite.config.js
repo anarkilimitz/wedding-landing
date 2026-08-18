aa67238580d9ca3f157ecfe0b53110c716ddf6f8
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-08-18 21:33:01 +0500

    Добавил vite-plugin-image-optimizer и настроил Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,32 @@ export default defineConfig({
 		react(),
 		babel({ presets: [reactCompilerPreset()] }),
 		tailwindcss(),
+
+		ViteImageOptimizer({
+			includePublic: true,
+
+			exclude: ['public/og-image.jpg'], // исключил чтоб не сжималась!!!!
+
+			png: {
+				quality: 80,
+			},
+
+			jpeg: {
+				quality: 70,
+			},
+
+			webp: {
+				quality: 75,
+			},
+
+			avif: {
+				quality: 60,
+			},
+
+			logStats: true,
+		}),
 	],
+
 	server: {
 		open: true,
 	},
